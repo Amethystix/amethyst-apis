87f0867d3697f618fd890fec36cd3f8eb3c33d1d
lad475 <lad475@nyu.edu>
2019-03-10 20:33:25 -0400

    Delete test file

--- test.js
+++
@@ -1,4 +0,0 @@
-const moment = require('moment');
-
-const momentMath = moment({hour: 7, minute: 5}).subtract(5, 'hour').subtract(5, 'minute');
-console.log(momentMath < moment({hour: 3, minute: 7}));
