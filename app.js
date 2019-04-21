0e5008d87373574495cb48c5a192f743f0022d80
lad475 <lad475@nyu.edu>
2019-04-21 01:17:10 -0400

    Adds CORS headers

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Cache-Control,Content-Type,Authorization');
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/amethyst', amethystRouter);
 app.use('/blog', blogRouter);
